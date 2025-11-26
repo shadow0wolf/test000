c30379245d6129d8b7bf80ae4e120e691c948d83
shadow0wolf <phreak911x@gmail.com>
2025-11-26 09:38:08 +0530

    commit 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-
+//initial file
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
